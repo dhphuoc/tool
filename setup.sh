f4d5069c52ff218b00259a24d75d6d717b2f8b99
Đàm Hữu Phước <153354626+dhphuoc@users.noreply.github.com>
2025-02-14 09:21:07 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+termux-setup-storage
+apt update && yes | apt upgrade -y
+pkg install python -y
+pkg install php -y
+pkg install nmap -y
+pkg install android-tools -y
+curl -o /sdcard/Download/dhp07_new.py https://dhphuoc21.x10.mx/dhp07main/dhp07.txt
+echo "alias dhp07='cd /sdcard/Download && python dhp07_new.py'" >> ~/.bashrc
+
+source ~/.bashrc
+
+printf '\n\033[1;32m Gõ \033[1;33dhp07 \033[1;32mĐể Vào Tool \n\n'
